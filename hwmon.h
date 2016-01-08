b7392cfb3a759535043125fc20e12b00b7d953c3
Bjørn Mork <bjorn@mork.no>
2016-01-08 22:51:14 +0100

    mwlwifi: add GPLv2 blob to hwmon files
    Signed-off-by: Bjørn Mork <bjorn@mork.no>

--- hwmon.h
+++ hwmon.h
@@ -1,5 +1,9 @@
 /*
  * Copyright (C) 2016  Bjørn Mork <bjorn@mork.no>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
  */
 
 #ifndef _MWL_HWMON_H_
